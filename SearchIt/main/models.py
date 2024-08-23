0263724573b3b15f9bd69e7fd6333faf49b649a0
Rajan Karki <rajankarkee8@gmail.com>
2024-08-23 15:49:34 +0900

    feat:Implemented search backend

--- SearchIt/main/models.py
+++ SearchIt/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Item(models.Model):
+    name = models.CharField(max_length=255, null=True, default="",blank=True)
+    image = models.ImageField(null=True, blank = True, upload_to='images/')
+    about = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+    
--- SearchIt/main/views.py
+++ SearchIt/main/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
+from django.db.models import Q
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    items = Item.objects.all()
+    context = {
+        'items':items
+    }
+    return render(request, 'main/home.html', context)
+
+
+def SearchItem(request):
+    
+    query = request.GET['query'] if 'query' in request.GET else '' 
+    items = Item.objects.filter(
+        Q(name__icontains = query)|
+        Q(about__icontains = query)|
+        Q(image__icontains = query))
+    context = {'query':query , 'items':items}
+    return render(request, 'main/search.html', context)
